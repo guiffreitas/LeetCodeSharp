da9c96b309af5719bc0f33cc0fe727904a52ce4e
Guilherme Faria <ffaria.gui@gmail.com>
2025-03-23 10:39:53 -0300

    LeetCode 167

--- Enums/ProblemCatalog.cs
+++ Enums/ProblemCatalog.cs
@@ -7,6 +7,7 @@
         P68_TextJustification,
         P88_MergeSortedArray,
         P125_ValidPalindrome,
+        P167_TwoSumII,
         P392_IsSubsequence,
     }
 }
---
+++ Problems/P167_TwoSumII.cs
@@ -0,0 +1,37 @@
+﻿using LeetCodeSharp.Interfaces;
+
+namespace LeetCodeSharp.Problems
+{
+    class P167_TwoSumII : IProblem
+    {
+        public void Run()
+        {
+            var numbers = new int[] { 2, 7, 11, 15 };
+            var target = 9;
+            var result = TwoSum(numbers, target);
+        }
+        public int[] TwoSum(int[] numbers, int target)
+        {
+
+            var sumDict = new Dictionary<int, int>();
+
+            var result = new int[2];
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                if (sumDict.Count > 0 && sumDict.ContainsKey(numbers[i]))
+                {
+                    result[0] = sumDict[numbers[i]] + 1;
+                    result[1] = i + 1;
+                    break;
+                }
+                else
+                {
+                    sumDict[target - numbers[i]] = i;
+                }
+            }
+
+            return result;
+        }
+    }
+}
